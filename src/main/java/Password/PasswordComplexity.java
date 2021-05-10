ce85e8daab3ee0aac7be26064c868eed3f0c5e2d
Britosl1 <lucasbrito.carneiro@gmail.com>
2021-05-09 22:24:23 -0300

    Testing commit 2

--- src/main/java/Password/PasswordComplexity.java
+++ src/main/java/Password/PasswordComplexity.java
@@ -1,4 +1,27 @@
 package Password;
 
-public class PasswordComplexity {
+
+public class  PasswordComplexity extends PasswordMeterV2 {
+    public String complexity;
+    public int score;
+
+     if (score > 100) {
+        score = 100;
+    } else if (score < 0) {
+        score = 0;
+    }
+
+     if (score < 20) {
+        complexity = "Very Weak";
+    } else if (score < 40) {
+        complexity = "Weak";
+    } else if (score < 60) {
+        complexity = "Good";
+    } else if (score < 80) {
+        complexity = "Strong";
+    } else {
+        complexity = "Very Strong";
+    }
+}
+
 }
