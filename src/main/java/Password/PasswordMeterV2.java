f40ca4e0e02fadfd5e3923ce1cf18d666dd9a569
Britosl1 <lucasbrito.carneiro@gmail.com>
2021-05-09 21:12:04 -0300

    Testing Commit

---
+++ src/main/java/Password/PasswordMeterV2.java
@@ -0,0 +1,14 @@
+package Password;
+
+public class PasswordMeterV2 {
+    //total score
+    public int score;
+    //complexity of the password
+    public String complexity;
+    //complexity class
+    private class measureComplexityOfPassword {
+
+    }
+    //password class
+    //
+}
--- src/main/java/br/com/lucasbrito/db1start/Main.java
+++
@@ -1,4 +0,0 @@
-package br.com.lucasbrito.db1start;
-
-public class Main {
-}
